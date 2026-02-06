1fbcca80beae6790b59630d178f2337a9f2f4ee3
jesuszruelas <jesusruelas685@gmail.com>
2026-02-06 07:18:54 -0700

    Proyecto Update

---
+++ CalculadoraCompletaJZ/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CalculadoraCompletaJZ
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CalculadoraCompletaJZ/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculadoraCompletaJZ
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
